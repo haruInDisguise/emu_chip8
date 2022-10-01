340727e68931cdde000b02eff878ed5a09c59ba7
haruInDisguise <justhoro@protonmail.com>
2022-10-01 19:14:09 +0200

    Added explicit 'extern' for consistency..

--- src/backend.h
+++ src/backend.h
@@ -3,11 +3,9 @@
 
 #include <stdint.h>
 
-#define CHIP8_BACKEND_PIXEL_MULT 20
-
-void CHIP8_backend_exit();
-uint32_t CHIP8_backend_init(void);
-uint32_t CHIP8_backend_render(void);
-uint32_t CHIP8_backend_handle_events(void);
+extern void CHIP8_backend_exit();
+extern uint32_t CHIP8_backend_init(void);
+extern uint32_t CHIP8_backend_render(void);
+extern uint32_t CHIP8_backend_handle_events(void);
 
 #endif
--- src/log.h
+++ src/log.h
@@ -1,30 +1,29 @@
 #ifndef _LOG_H_
 #define _LOG_H_
 
-enum LOG_LEVEL {
+typedef enum {
     LOG_LEVEL_ALL = 0,
     LOG_LEVEL_DEBUG,
     LOG_LEVEL_INFO,
     LOG_LEVEL_WARN,
     LOG_LEVEL_ERROR,
     LOG_LEVEL_NONE,
-};
+} LOG_LEVEL;
 
-typedef enum LOG_LEVEL LOG_LEVEL;
 typedef void(log_func_t)(LOG_LEVEL level, char *msg);
 
-void log_init(void);
+extern void log_init(void);
 
 /// Register a callback for <level>
 // TODO: This seems to be the most logical/practical implementation... ?
 //       Also, this should be it's own library
-void log_register(LOG_LEVEL level, log_func_t func);
+extern void log_register(LOG_LEVEL level, log_func_t func);
 
 /// Set the desired log level at runtime
-void log_set_level(LOG_LEVEL level);
+extern void log_set_level(LOG_LEVEL level);
 
 /// Invoke the appropiate callback
-void log_emit(LOG_LEVEL level, char *format, ...);
+extern void log_emit(LOG_LEVEL level, char *format, ...);
 
 #define log_debug(format, ...) log_emit(LOG_LEVEL_DEBUG, format, __VA_ARGS__)
 #define log_info(format, ...)  log_emit(LOG_LEVEL_INFO, format, __VA_ARGS__)
